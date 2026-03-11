f88c3fe3a04cfab8e4290b6cedebe6fb0efdfd21
CesarUTAGS <210003@utags.edu.mx>
2026-03-11 11:51:42 -0600

    Proyecto base con 4 paginas y servicios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,35 @@
 import { Routes } from '@angular/router';
 
+
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+ 
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'details',
+    loadComponent: () => import('./pages/details/details.page').then( m => m.DetailsPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
   },
 ];
